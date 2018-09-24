fa75912c685c3c70cba71dae6ff907526af66210
Harry Chen <harrychen@loop-depaulsecure-253-249.depaulsecure-student.depaul.edu>
2018-09-24 14:35:04 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './mainroot/header/header.component';
+import { NavigationComponent } from './mainroot/navigation/navigation.component';
+import { MaincontentComponent } from './mainroot/maincontent/maincontent.component';
+import { FooterComponent } from './mainroot/footer/footer.component';
+import { MaintainComponent } from './mainroot/maintain/maintain.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MainrootComponent } from './mainroot/mainroot.component';
 
+
+const appRoute: Routes = [
+  {path: '', component: MainrootComponent, 
+   children: [
+   ]},
+   {path: 'maintaining', component: MaintainComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    MaincontentComponent,
+    FooterComponent,
+    MaintainComponent,
+    MainrootComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mainroot/mainroot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mainroot',
+  templateUrl: './mainroot.component.html',
+  styleUrls: ['./mainroot.component.scss']
+})
+export class MainrootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
